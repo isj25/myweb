fad59a666791991c2a95cd2abc97245464e72a98
isj25 <ishwarj515@gmail.com>
2022-07-29 10:56:47 +0530

    added upcoming

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 
 
 const myName = "Ishwar Joshi"
-
+const upcoming = "Wait !!! Writing More Code (-)"
 const myNameElement= document.getElementById("name");
+const upcomingElement = document.querySelector(".upcoming")
 const ABOUTME = "ABOUT ME"
 
 let letter = ""
-let letter2 =""
+let letter_upcoming =""
 let index  = 0;
-
-(function typing(){
+let index2 = 0;
+function typing_name(){
     letter  = myName.slice(0,++index);
-    
+   
     if(index=== myName.length)
     {
         // index = 0;
@@ -20,14 +21,32 @@ let index  = 0;
     }else
     {
         myNameElement.textContent = letter + "_"
-        setTimeout(typing,200)
+       
+        setTimeout(typing_name,200)
     }
     
 
-
-})()
-
+   
 
 
+}
+
+typing_name()
 
 
+
+
+
+function alwaysTyping(){
+    letter_upcoming = upcoming.slice(0,++index2)
+    
+    upcomingElement.textContent = letter_upcoming +'_'
+    if(index2 == upcoming.length)
+    {
+        index2 = 0;
+    }
+    setTimeout(alwaysTyping,200)
+}
+
+
+alwaysTyping()
